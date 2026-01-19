53813e43df420158711afbf21c62c9895c6ec183
unknown <designwithtawhid@gmail.com>
2026-01-19 22:23:49 +0600

    updated google login system

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
+import { authClient } from "@/lib/auth-client";
+
+export default async function Home() {
+  const session = await authClient.getSession();
+  console.log(session);
   return <h2>Hellow word</h2>;
 }
